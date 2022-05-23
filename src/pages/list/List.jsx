e45930958d5c7ab43e963a6445b9dfdfb2e660de
meir <lightir21@gmail.com>
2022-05-23 18:01:59 +0300

    search item

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -5,6 +5,7 @@ import Navbar from "../../components/navbar/Navbar";
 import { useLocation } from "react-router-dom";
 import { format } from "date-fns";
 import { DateRange } from "react-date-range";
+import SearchItem from "../../components/searchItem/SearchItem";
 
 const List = () => {
   const location = useLocation();
@@ -41,45 +42,60 @@ const List = () => {
             </div>
             <div className="lsItem">
               <label>Options</label>
-              <div className="lsOptionItem">
-                <span className="lsOptionText">
-                  Min price <small>pet night</small>
-                </span>
-                <input type="number" className="lsOptionInput" />
-              </div>
-              <div className="lsOptionItem">
-                <span className="lsOptionText">
-                  Max price <small>pet night</small>
-                </span>
-                <input type="number" className="lsOptionInput" />
-              </div>
-              <div className="lsOptionItem">
-                <span className="lsOptionText">Adult</span>
-                <input
-                  type="number"
-                  className="lsOptionInput"
-                  placeholder={options.adult}
-                />
-              </div>
-              <div className="lsOptionItem">
-                <span className="lsOptionText">Children</span>
-                <input
-                  type="number"
-                  className="lsOptionInput"
-                  placeholder={options.children}
-                />
-              </div>
-              <div className="lsOptionItem">
-                <span className="lsOptionText">Room</span>
-                <input
-                  type="number"
-                  className="lsOptionInput"
-                  placeholder={options.room}
-                />
+              <div className="lsOptions">
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">
+                    Min price <small>per night</small>
+                  </span>
+                  <input type="number" className="lsOptionInput" />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">
+                    Max price <small>pet night</small>
+                  </span>
+                  <input type="number" className="lsOptionInput" />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Adult</span>
+                  <input
+                    type="number"
+                    min={1}
+                    className="lsOptionInput"
+                    placeholder={options.adult}
+                  />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Children</span>
+                  <input
+                    type="number"
+                    min={0}
+                    className="lsOptionInput"
+                    placeholder={options.children}
+                  />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Room</span>
+                  <input
+                    type="number"
+                    min={1}
+                    className="lsOptionInput"
+                    placeholder={options.room}
+                  />
+                </div>
               </div>
             </div>
+            <button>Search</button>
           </div>
-          <div className="listResult"></div>
+          <div className="listResult">
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+          </div>
         </div>
       </div>
     </div>
