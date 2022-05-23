62f6b0f999ba49f9e85ad822f83cc368a26fc85f
meir <lightir21@gmail.com>
2022-05-23 12:29:50 +0300

    most of the design

---
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import "./featuredProperties.css";
+
+const FeaturedProperties = () => {
+  return (
+    <div className="fp">
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/max500/75328633.jpg?k=87304e5542c63c022f2cbc134b02b85b65496a9ed8c6ca129b49c02f817589db&o="
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Aparthotel Stare Miasto</span>
+        <span className="fpCity">Madrid</span>
+        <span className="fpPrice">Starting from $120</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/max500/75328633.jpg?k=87304e5542c63c022f2cbc134b02b85b65496a9ed8c6ca129b49c02f817589db&o="
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Aparthotel Stare Miasto</span>
+        <span className="fpCity">Madrid</span>
+        <span className="fpPrice">Starting from $120</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/max500/75328633.jpg?k=87304e5542c63c022f2cbc134b02b85b65496a9ed8c6ca129b49c02f817589db&o="
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Aparthotel Stare Miasto</span>
+        <span className="fpCity">Madrid</span>
+        <span className="fpPrice">Starting from $120</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/max500/75328633.jpg?k=87304e5542c63c022f2cbc134b02b85b65496a9ed8c6ca129b49c02f817589db&o="
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">Aparthotel Stare Miasto</span>
+        <span className="fpCity">Madrid</span>
+        <span className="fpPrice">Starting from $120</span>
+        <div className="fpRating">
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProperties;
---
+++ src/pages/list/List.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import "./list.css";
+import Header from "../../components/header/Header";
+import Navbar from "../../components/navbar/Navbar";
+import { useLocation } from "react-router-dom";
+import { format } from "date-fns";
+import { DateRange } from "react-date-range";
+
+const List = () => {
+  const location = useLocation();
+  const [destination, setDestination] = useState(location.state.destination);
+  const [date, setDate] = useState(location.state.date);
+  const [openDate, setOpenDate] = useState(false);
+  const [options, setOptions] = useState(location.state.options);
+  console.log(location);
+  return (
+    <div>
+      <Navbar />
+      <Header type="list" />
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+            <h1 className="lsTitle">Search</h1>
+            <div className="lsItem">
+              <label>Destination</label>
+              <input type="text" placeholder={destination} />
+            </div>
+            <div className="lsItem">
+              <label>Check-in Date</label>
+              <span onClick={() => setOpenDate(!openDate)}>{`${format(
+                date[0].startDate,
+                "MM/dd/yyyyy"
+              )} to ${format(date[0].endDate, "MM/dd/yyyyy")}`}</span>
+              {openDate && (
+                <DateRange
+                  onChange={(item) => setDate([item.selection])}
+                  minDate={new Date()}
+                  ranges={date}
+                />
+              )}
+            </div>
+            <div className="lsItem">
+              <label>Options</label>
+              <div className="lsOptionItem">
+                <span className="lsOptionText">
+                  Min price <small>pet night</small>
+                </span>
+                <input type="number" className="lsOptionInput" />
+              </div>
+              <div className="lsOptionItem">
+                <span className="lsOptionText">
+                  Max price <small>pet night</small>
+                </span>
+                <input type="number" className="lsOptionInput" />
+              </div>
+              <div className="lsOptionItem">
+                <span className="lsOptionText">Adult</span>
+                <input
+                  type="number"
+                  className="lsOptionInput"
+                  placeholder={options.adult}
+                />
+              </div>
+              <div className="lsOptionItem">
+                <span className="lsOptionText">Children</span>
+                <input
+                  type="number"
+                  className="lsOptionInput"
+                  placeholder={options.children}
+                />
+              </div>
+              <div className="lsOptionItem">
+                <span className="lsOptionText">Room</span>
+                <input
+                  type="number"
+                  className="lsOptionInput"
+                  placeholder={options.room}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="listResult"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default List;
